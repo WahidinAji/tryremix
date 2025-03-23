fc5c6bc9dddec91fdf44d926ce09083239980b05
WahidinAji <a17wahidin@gmail.com>
2025-03-23 21:19:39 +0700

    remove all, lets begin again

--- app/routes/blog.tsx
+++
@@ -1,9 +0,0 @@
-export default function Blog() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <h1>Blog</h1>
-      </div>
-    </div>
-  );
-}
--- app/routes/pricing.tsx
+++
@@ -1,9 +0,0 @@
-export default function Pricing() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <h1>Pricing</h1>
-      </div>
-    </div>
-  );
-}
